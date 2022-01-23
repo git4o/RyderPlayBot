46635aa31d2147360669b3fbf0360d4d538fb31d
git4o <71034923+git4o@users.noreply.github.com>
2022-01-23 07:30:47 -0800

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,44 @@
+
+                (async()=>{
+                const Discord = require("discord.js");
+                const Database = require("easy-json-database");
+                const devMode = typeof __E_IS_DEV !== "undefined" && __E_IS_DEV;
+                const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+                const s4d = {
+                    Discord,
+                    database: new Database(`${devMode ? S4D_NATIVE_GET_PATH : "."}/db.json`),
+                    joiningMember:null,
+                    reply:null,
+                    tokenInvalid:false,
+                    tokenError: null,
+                    checkMessageExists() {
+                        if (!s4d.client) throw new Error('You cannot perform message operations without a Discord.js client')
+                        if (!s4d.client.readyTimestamp) throw new Error('You cannot perform message operations while the bot is not connected to the Discord API')
+                    }
+                };
+                s4d.client = new s4d.Discord.Client({
+                    intents: [Object.values(s4d.Discord.Intents.FLAGS).reduce((acc, p) => acc | p, 0)],
+                    partials: ["REACTION"]
+                });
+
+                var arguments2, command, name2, yes;
+
+
+await s4d.client.login(process.env.token).catch((e) => { s4d.tokenInvalid = true; s4d.tokenError = e; });
+
+s4d.client.on('messageCreate', async (s4dmessage) => {
+  arguments2 = (s4dmessage.content).split(' ');
+  command = arguments2.splice(0, 1)[0];
+  if (command == '!play') {
+    name2 = (s4dmessage.member);
+    yes = arguments2.join(' ');
+    s4dmessage.channel.send(String('OKI DOKI!'));
+    yes.send(String((String(name2) + 'Wants To Play With You')));
+  }
+
+});
+
+
+                return s4d;
+                })();
+            
